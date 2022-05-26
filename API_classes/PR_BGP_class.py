9f82dbb459c6454206690cfdeba6600270daeadd
alberc01 <alberc01@ucm.es>
2022-05-26 13:56:17 +0200

    Correction on the bgp regex filter

--- API_classes/PR_BGP_class.py
+++ API_classes/PR_BGP_class.py
@@ -38,7 +38,7 @@ class PR_BGP(BGP):
             if item.replace('AS','').isnumeric():
                 return item
             else:
-                print(item + "is not valid")
+                print(item + " is not valid")
 
     def get_ASN_info_to_recommend(self):
         asn_dict = {}
@@ -64,38 +64,39 @@ class PR_BGP(BGP):
                             asn_dict[asn_key]['dates_ot'].append(k)
                             asn_dict[asn_key]['moredetail'] = important_Data
 
+                # TODO ¿Hace falta utilizar la informacon de los AS que son perjudicados en Hijack para las funciones de recomendacion?
                 # Informacion ASN para los hijacks perjudicados
-                for k,v in self.raw_data[primary_k]['HJ_by_date']['injured'].items():
-                    for item in v:
-                        asn_inj_ctry = item['ctry_inj']
-                        asn_cau_ctry = item['ctry_cau']
-                        asn_cau_key = self.get_correct_asn(item['ASN_cau'])
-                        asn_inju_key = self.get_correct_asn(item['ASN_inj'])
+                # for k,v in self.raw_data[primary_k]['HJ_by_date']['injured'].items():
+                #     for item in v:
+                #         asn_inj_ctry = item['ctry_inj']
+                #         asn_cau_ctry = item['ctry_cau']
+                #         asn_cau_key = self.get_correct_asn(item['ASN_cau'])
+                #         asn_inju_key = self.get_correct_asn(item['ASN_inj'])
 
-                        important_Data = item['scrapped_from_url']
+                #         important_Data = item['scrapped_from_url']
                 
-                        if asn_inju_key not in asn_dict.keys():
-                            asn_dict[asn_inju_key] = {
-                                'dates_ot': [],
-                                'dates_hj': [k],
-                                'country': asn_inj_ctry,
-                                'moredetail': important_Data               
-                                }
-                        else:
-                            asn_dict[asn_inju_key]['dates_hj'].append(k)
-                            asn_dict[asn_inju_key]['moredetail'] = important_Data
+                #         if asn_inju_key not in asn_dict.keys():
+                #             asn_dict[asn_inju_key] = {
+                #                 'dates_ot': [],
+                #                 'dates_hj': [k],
+                #                 'country': asn_inj_ctry,
+                #                 'moredetail': important_Data               
+                #                 }
+                #         else:
+                #             asn_dict[asn_inju_key]['dates_hj'].append(k)
+                #             asn_dict[asn_inju_key]['moredetail'] = important_Data
 
-                        if asn_cau_key not in asn_dict.keys():
-                            asn_dict[asn_cau_key] = {
-                                'dates_ot': [],
-                                'dates_hj': [k],
-                                'country': asn_cau_ctry,
-                                'moredetail': important_Data              
-                                }
-                        else:
-                            asn_dict[asn_cau_key]['dates_hj'].append(k)
-                            asn_dict[asn_cau_key]['moredetail'] = important_Data
-
+                #         if asn_cau_key not in asn_dict.keys():
+                #             asn_dict[asn_cau_key] = {
+                #                 'dates_ot': [],
+                #                 'dates_hj': [k],
+                #                 'country': asn_cau_ctry,
+                #                 'moredetail': important_Data              
+                #                 }
+                #         else:
+                #             asn_dict[asn_cau_key]['dates_hj'].append(k)
+                #             asn_dict[asn_cau_key]['moredetail'] = important_Data
+        
                 # Informacion ASN para los hijacks causantes
                 for k,v in self.raw_data[primary_k]['HJ_by_date']['causer'].items():
                     for item in v:
@@ -158,12 +159,12 @@ class PR_BGP(BGP):
         commands = self.construct_vtysh_commands_ot(asn_cau, pref_to_reduce)
         return pref_to_reduce,commands
 
-    #TODO Funcion para evaluar los secuestros
+    # Funcion para evaluar los secuestros
     def get_HJ_evaluation(self, asn_selected):
         asn_hj = self.get_incidents_by_month(self.asn_data[asn_selected]['dates_hj'])
         return asn_hj
 
-    #TODO Funcion para evaluar las caidas de servicio
+    # Funcion para evaluar las caidas de servicio
     def get_OT_evaluation(self, asn_selected):
         asn_ot = self.get_incidents_by_month(self.asn_data[asn_selected]['dates_ot'])
         return asn_ot
@@ -174,10 +175,11 @@ class PR_BGP(BGP):
             data.sort()
             month_constant = 30
             window_size = ut.string_to_datetime(data[-1]) - ut.string_to_datetime(data[0])
-            window_size = int(window_size.days/month_constant)+1
+            window_size = int((window_size.days)/month_constant)+1
             evaluation = [0]*window_size
             month_idx = 0
             date_out = ut.string_to_datetime(data[0])
+            first_date = date_out
             evaluation[month_idx] += 1 
             for i in range(1,len(data)):
                 date_out_next = ut.string_to_datetime(data[i])
@@ -186,7 +188,8 @@ class PR_BGP(BGP):
                     evaluation[month_idx] += 1 
                 else:
                     date_out = date_out_next
-                    month_idx += 1
+                    total_time = date_out - first_date
+                    month_idx = int(total_time.days/30)                  
                     evaluation[month_idx] += 1 
 
             return evaluation
@@ -201,30 +204,30 @@ class PR_BGP(BGP):
 ! Router section   \n\
 router bgp <YOUR ASN NUMBER> \n\
     ! <a.b.c.d> is the IP direction of the router BGP neighbour\n\
-    neighbour <a.b.c.d> remote_as " + asn.replace('AS','') + "\n\
-    neighbour <a.b.c.d> route-map BgpRS_RM " + asn.replace('AS','')  + " in \n\
-    ! Access-list declaration section \n\
-        ip access-list BgpRS_RECOM permit "+ asn_filter+ " !Filter to find all routes in AS_PATH where the AS problem causer is between \n\
-    ! Route-map declaration section \n\
-        route-map BgpRS_RM permit 10 \n\
-            match as-path BgpRS_RECOM\n\
-            set local-prefence (- " + str(pref)+ ")" 
+    neighbor <a.b.c.d> remote_as " + asn.replace('AS','') + "\n\
+    neighbor <a.b.c.d> route-map BgpRS_RM " + asn.replace('AS','')  + " in \n\
+! Access-list declaration section \n\
+ip access-list BgpRS_RECOM permit "+ asn_filter+ " !Filter to find all routes in AS_PATH where the AS problem causer is between \n\
+! Route-map declaration section \n\
+route-map BgpRS_RM permit 10 \n\
+    match as-path BgpRS_RECOM\n\
+    set local-prefence (- " + str(pref)+ ")" 
         return string
     
 
     def construct_vtysh_commands_hj(self, asn, pref):
         # El filtrado solo debe contemplar aquellos anuncios en los que el ASN es origen o final
-        asn_filter = '_' + asn.replace('AS','') + '$' # Rutas con origen en el ASN agresor
+        asn_filter = asn.replace('AS','') + '$' # Rutas con origen en el ASN agresor
         string = "\
 ! Router section   \n\
 router bgp <YOUR ASN NUMBER> \n\
     ! <a.b.c.d> is the IP direction of the router BGP neighbour\n\
-    neighbour <a.b.c.d> remote_as " + asn.replace('AS','') + "\n\
-    neighbour <a.b.c.d> route-map BgpRS_RM " + asn.replace('AS','') + " in \n\
-    ! Access-list declaration section \n\
-        ip access-list BgpRS_RECOM permit "+ asn_filter + " ! Filter to find all routes in AS_PATH where the AS problem is origin \n\
-    ! Route-map declaration section \n\
-        route-map BgpRS_RM permit 10 \n\
-        match as-path BgpRS_RECOM\n\
-        set local-prefence (- " + str(pref)+ ")" 
+    neighbor <a.b.c.d> remote_as " + asn.replace('AS','') + "\n\
+    neighbor <a.b.c.d> route-map BgpRS_RM " + asn.replace('AS','') + " in \n\
+! Access-list declaration section \n\
+ip access-list BgpRS_RECOM permit "+ asn_filter + " ! Filter to find all routes in AS_PATH where the AS problem is origin \n\
+! Route-map declaration section \n\
+route-map BgpRS_RM permit 10 \n\
+    match as-path BgpRS_RECOM\n\
+    set local-prefence (- " + str(pref)+ ")" 
         return string
